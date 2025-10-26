30cc61a1a999d08caea2cf6ae75f0660da52734c
Fahmi Aga Aditya <fahmi.aga@gmail.com>
2025-10-26 15:19:12 +0700

    Improve  session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,4 @@
-Rails.application.config.session_store :cookie_store, key: "_your_app_session"
+Rails.application.config.session_store :cookie_store,
+  key: "_chat_app_session",
+  same_site: :none,
+  secure: true
